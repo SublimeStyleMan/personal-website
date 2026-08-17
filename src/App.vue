4e41c3d9c2fb0e6d09f8538d331f06a43a4d6894
SublimeStyleMan <Nathan.swindall20103@gmail.com>
2026-08-17 12:44:57 +0000

    Added a navbar to the page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 import Navbar from "./components/Navbar.vue";
-import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <Navbar />
-  <main class="min-h-screen bg-gray-50">
-    <HelloWorld />
-  </main>
 </template>
